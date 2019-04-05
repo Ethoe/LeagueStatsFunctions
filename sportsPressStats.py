6e46a0b5890eb5329b9d75b3c12eddc3ec3ec078
Ryan Outtrim <ryanouttrim@gmail.com>
2019-04-04 19:57:02 -0600

    Finished full sportspress csv exporter, to zip into exe file

--- sportsPressStats.py
+++ sportsPressStats.py
@@ -19,8 +19,6 @@ class Player:
     gold: int = 0
     cs: int = 0
     dmgDealt: int = 0
-    teamDmg: int = 0
-    teamKills: int = 0
     playerScore: float = 0
 
 
@@ -45,16 +43,34 @@ def getTeam(filename):
             if counter == 0:
                 matchID = row[1]
                 counter += 1
+            elif counter == 1:
+                date = row[1]
+                counter += 1
+            elif counter == 2:
+                time = row[1]
+                counter += 1
+            elif counter == 3:
+                blueTeamName = row[1]
+                if row[0] == str('Home'):
+                    home = True
+                counter += 1
+            elif counter == 4:
+                redTeamName = row[1]
+                if row[0] == str('Home'):
+                    home = False
+                counter += 1
             else:
                 champNumber = champions[row[1]]['key']
                 players[row[0]] = champNumber
 
-    return players, matchID
+    return players, matchID, date, time, blueTeamName, redTeamName, home
 
 
 def statGatherer(players, matchID, apiKey):
     blueTeam = dict()
     redTeam = dict()
+    blueStat = [0, 0, 0, 0, 0]
+    redStat = [0, 0, 0, 0, 0]
     counter = 0
     for player in players:
         if counter < 5:
@@ -68,10 +84,7 @@ def statGatherer(players, matchID, apiKey):
     matchUrl = 'https://na1.api.riotgames.com/lol/match/v4/matches/' + matchID + '?api_key=' + apiKey
     request = requests.get(matchUrl)
     jsonData = request.json()
-    blueDmg = 0
-    redDmg = 0
-    blueKills = 0
-    redKills = 0
+
     for participant in range(10):
         champPlayed = int(jsonData['participants'][participant]['championId'])
         jsonPlayer = jsonData['participants'][participant]['stats']
@@ -80,26 +93,25 @@ def statGatherer(players, matchID, apiKey):
             if champPlayed is int(gamer.champNumber):
                 gamer.ID = participant
                 playerMaker(jsonPlayer, gamer)
-                blueKills += gamer.kills
-                blueDmg += gamer.dmgDealt
-        for user in blueTeam:
-            gamer = blueTeam[user]
-            gamer.teamDmg = blueDmg
-            gamer.teamKills = blueKills
+                blueStat[0] += gamer.dmgDealt
+                blueStat[1] += gamer.kills
+                blueStat[2] += gamer.deaths
+                blueStat[3] += gamer.assists
+                blueStat[4] += gamer.gold
 
         for user in redTeam:
             gamer = redTeam[user]
             if champPlayed == int(gamer.champNumber):
                 gamer.ID = participant
                 playerMaker(jsonPlayer, gamer)
-                redKills += gamer.kills
-                redDmg += gamer.dmgDealt
-        for user in redTeam:
-            gamer = redTeam[user]
-            gamer.teamDmg = redDmg
-            gamer.teamKills = redKills
+                redStat[0] += gamer.dmgDealt
+                redStat[1] += gamer.kills
+                redStat[2] += gamer.deaths
+                redStat[3] += gamer.assists
+                redStat[4] += gamer.gold
 
-    return blueTeam, redTeam
+    blueWin = jsonData['teams'][0]['win']
+    return blueTeam, redTeam, blueWin, blueStat, redStat
 
 
 def playerMaker(jsonPlayer, gamer):
@@ -127,51 +139,72 @@ def playerMaker(jsonPlayer, gamer):
     return gamer
 
 
-def csvReturn(blueTeam, redTeam):
-    with open('playerStats.csv', mode='w') as csvFile:
-        fields = ['Player Name', 'Kills', 'Deaths', 'Assists', 'Gold', 'CS', 'Damage Dealt', 'Player Score',
-                  'Team Damage', 'Team Kills']
+def resultMaker(totalDmg, totalKills, totalDeaths, totalAssists, totalGold):
+    return ' | '.join([str(totalDmg), str(totalKills), str(totalDeaths), str(totalAssists), str(totalGold)])
+
+
+def csvReturn(blueTeam, redTeam, date, time, blueTeamName, redTeamName, firstWin, blueStat, redStat):
+    with open('match.csv', mode='w') as csvFile:
+        fields = ['Date', 'Time', 'Venue', 'Teams', 'Results', 'Outcome',
+                  'Players', 'CS', 'Damage Dealt', 'Kills', 'Deaths', 'Assists', 'Gold']
+
         writer = csv.DictWriter(csvFile, fieldnames=fields, delimiter=',', lineterminator='\n')
         writer.writeheader()
+
+        if firstWin:
+            teamOne = 'Win'
+            teamTwo = 'Loss'
+        else:
+            teamOne = 'Loss'
+            teamTwo = 'Win'
+
+        counter = 0
         for user in blueTeam:
             gamer = blueTeam[user]
-            row = dict()
-            row[fields[0]] = gamer.name
-            row[fields[1]] = gamer.kills
-            row[fields[2]] = gamer.deaths
-            row[fields[3]] = gamer.assists
-            row[fields[4]] = gamer.gold
-            row[fields[5]] = gamer.cs
-            row[fields[6]] = gamer.dmgDealt
-            row[fields[7]] = gamer.playerScore
-            row[fields[8]] = gamer.teamDmg
-            row[fields[9]] = gamer.teamKills
-            writer.writerow(row)
+            if counter == 0:
+                result = resultMaker(blueStat[0], blueStat[1], blueStat[2], blueStat[3], blueStat[4])
+                writer.writerow({fields[0]: date, fields[1]: time, fields[2]: 'Online', fields[3]: blueTeamName,
+                                 fields[4]: result, fields[5]: teamOne, fields[6]: gamer.name,
+                                 fields[7]: gamer.cs, fields[8]: gamer.dmgDealt, fields[9]: gamer.kills,
+                                 fields[10]: gamer.deaths, fields[11]: gamer.assists, fields[12]: gamer.gold})
+                counter += 1
+            else:
+                writer.writerow({fields[6]: gamer.name, fields[7]: gamer.cs, fields[8]: gamer.dmgDealt,
+                                 fields[9]: gamer.kills, fields[10]: gamer.deaths, fields[11]: gamer.assists,
+                                 fields[12]: gamer.gold})
+
+        counter = 0
         for user in redTeam:
             gamer = redTeam[user]
-            row = dict()
-            row[fields[0]] = gamer.name
-            row[fields[1]] = gamer.kills
-            row[fields[2]] = gamer.deaths
-            row[fields[3]] = gamer.assists
-            row[fields[4]] = gamer.gold
-            row[fields[5]] = gamer.cs
-            row[fields[6]] = gamer.dmgDealt
-            row[fields[7]] = gamer.playerScore
-            row[fields[8]] = gamer.teamDmg
-            row[fields[9]] = gamer.teamKills
-            writer.writerow(row)
+            if counter == 0:
+                result = resultMaker(redStat[0], redStat[1], redStat[2], redStat[3], redStat[4])
+                writer.writerow({fields[3]: redTeamName, fields[4]: result, fields[5]: teamTwo,
+                                 fields[6]: gamer.name, fields[7]: gamer.cs, fields[8]: gamer.dmgDealt,
+                                 fields[9]: gamer.kills, fields[10]: gamer.deaths, fields[11]: gamer.assists,
+                                 fields[12]: gamer.gold})
+                counter += 1
+            else:
+                writer.writerow({fields[6]: gamer.name, fields[7]: gamer.cs, fields[8]: gamer.dmgDealt,
+                                 fields[9]: gamer.kills, fields[10]: gamer.deaths, fields[11]: gamer.assists,
+                                 fields[12]: gamer.gold})
+
     return
 
 
 def driver():
     apiKey = 'RGAPI-dd1500ad-117d-476a-9bca-51029a0ec6b8'
-    game = getTeam("match.csv")
+    game = getTeam("inputMatch.csv")
     stats = statGatherer(game[0], game[1], apiKey)
-    csvReturn(stats[0], stats[1])
-    print(stats[0])
-    print('----------------------')
-    print(stats[1])
+    if game[6]:
+        if stats[2] == 'Win':
+            csvReturn(stats[0], stats[1], game[2], game[3], game[4], game[5], True, stats[3], stats[4])
+        else:
+            csvReturn(stats[0], stats[1], game[2], game[3], game[4], game[5], False, stats[3], stats[4])
+    else:
+        if stats[2] == 'Win':
+            csvReturn(stats[1], stats[0], game[2], game[3], game[5], game[4], False, stats[4], stats[3])
+        else:
+            csvReturn(stats[1], stats[0], game[2], game[3], game[5], game[4], True, stats[4], stats[3])
 
 
 driver()
